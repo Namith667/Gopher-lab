26047e69441942c56421aca4d27bc2e37969e2e9
namiththayyil@gmail.com <namiththayyil@gmail.com>
2024-09-04 22:07:57 +0530

    added shuffle func with rand

--- Cards/main.go
+++ Cards/main.go
@@ -1,11 +1,18 @@
 package main
 
-// import "fmt"
+
 
 //import "fmt"
 func main(){
 	cards := newDeckFromFile("my_cards")
+	cards.shuffle()
 	cards.print()
+	
+
+	//a := [...]int{1, 3, 4, 5, 6, 6}
+	
+	//fmt.Println(a[rand.Intn(len(a))])
+
 	// hand,remainingCards := deal(cards,5)
 	// hand.print()
 	// remainingCards.print()
