d47dd2c5fe9c4fa39d92b14ae2bd5a76e0f35785
Namith667 <namiththayyil@gmail.com>
2024-10-14 12:24:36 +0530

    Used joho/godotenv

--- WebServer/main.go
+++ WebServer/main.go
@@ -1,54 +1,41 @@
 package main
 
 import (
+	"WebServer/models"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-)
+	"os"
 
-type RequestData struct {
-	Name  string `json:"name"`
-	Age   int    `json:"age"`
-	Email string `son:"age"`
-}
-type ResponseData struct {
-	Message string `json:"message"`
-	Status  string `json:"status"`
-}
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	http.HandleFunc(`/api`, handleRequest)
-	http.HandleFunc(`/status`, handleStatus)
-	fmt.Println("Server running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func handleStatus(w http.ResponseWriter, r *http.Request) {
-	responseData := ResponseData{
-		Message: "Server is running Successfully",
-		Status:  "OK!",
-	}
-	w.Header().Set("Context-Type", "application.json")
-	responseJSON, err := json.Marshal(responseData)
+	err := godotenv.Load()
+	port := os.Getenv("PORT")
 	if err != nil {
-		http.Error(w, "Error parsing json data ", http.StatusInternalServerError)
-		return
+		log.Fatalf("Cannot load")
 	}
-	w.Write(responseJSON)
+	fmt.Println("Server running on port 8080")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
-
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid post method", http.StatusMethodNotAllowed)
+		return
+	}
 	//Parse body
-	var requestData RequestData
+	var requestData models.RequestData
 	err := json.NewDecoder(r.Body).Decode(&requestData)
 	if err != nil {
 		http.Error(w, "Error parsing body", http.StatusBadRequest)
 		return
 	}
 	//create response
-	responseData := ResponseData{
+	responseData := models.ResponseData{
 		Message: fmt.Sprintf("Hello %x ! We have recieved your data. Your age is : %v and email id is: %v .", requestData.Name, requestData.Age, requestData.Email),
 		Status:  "Success",
 	}
